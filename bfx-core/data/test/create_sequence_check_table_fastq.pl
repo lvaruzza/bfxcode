b5af1e7947b3fbdeb39d3088015da0f3bec862bc
Leonardo Varuzza <varuzza@gmail.com>
2011-11-17 00:25:16 -0200

    Add test for fastq reader

--- bfx-core/data/test/create_sequence_check_table_fastq.pl
+++ bfx-core/data/test/create_sequence_check_table_fastq.pl
@@ -5,8 +5,8 @@ $in  = Bio::SeqIO->new(-file => $ARGV[0] , '-format' => 'Fastq');
 
 while ( my $seq = $in->next_seq() ) {
     print $seq->id(),"\t";
-    print md5_hex($seq->qual_text()),"\t";
-    print md5_hex($seq->seq),"\n";
+    print md5_hex($seq->seq),"\t";
+    print md5_hex($seq->qual_text()),"\n";
     #print $seq->primary_id,"\t";
     #print $seq->description,"\t";
 }
--- bfx-core/src/test/java/bfx/io/impl/TestLineBasedFastQIterator.java
+++ bfx-core/src/test/java/bfx/io/impl/TestLineBasedFastQIterator.java
@@ -1,7 +1,10 @@
 package bfx.io.impl;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import java.io.IOException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Iterator;
 
 import org.apache.log4j.Logger;
@@ -36,38 +39,12 @@ public class TestLineBasedFastQIterator {
 		Sequence seq1 = it.next();
 		log.debug(seq1);
 		assertEquals("ACGT",seq1.getSeqAsString());
-		//assertEquals("1 2 3 4",seq1.getQualAsString());
-	}
-	/*
-	@Test
-	public void testReadFromString() throws IOException {
-		Iterator<Sequence> it = reader.readString(
-				">1\n" +
-				"ACGT\n" +
-				"CGTT\n" +
-				">2\n" +
-				"CCCCCC\n",
-				">1\n" +
-				"0 1 2 3\n" +
-				"4 5 6 7\n" +
-				">2\n" +
-				"9 9 9 9 9 9");
-		
-		Sequence seq1 = it.next();
-		log.debug(seq1);		
-		assertEquals("1",seq1.getId());
-		assertEquals("ACGTCGTT",seq1.getSeqAsString());
-		
-		Sequence seq2 = it.next();
-		assertEquals("2",seq2.getId());
-		assertEquals("CCCCCC",seq2.getSeqAsString());
-		log.debug(seq2);
+		assertEquals("20 20 20 22",seq1.getQualAsString());
 	}
 	
-	
-	private void testReadFromFile(String seqFilename,String qualFilename,String checkname) throws IOException, NoSuchAlgorithmException {
+	private void testReadFromFile(String seqFilename,String checkname) throws IOException, NoSuchAlgorithmException {
 		//log.setLevel(Level.INFO);
-		Iterator<Sequence> it = reader.read(seqFilename,qualFilename);
+		Iterator<Sequence> it = reader.read(seqFilename);
 		Iterator<String[]> check = tblReader.read(checkname);
 		while(it.hasNext()) {
 			assertTrue(check.hasNext());
@@ -75,22 +52,19 @@ public class TestLineBasedFastQIterator {
 			Sequence seq = it.next();
 			assertEquals(chk[0],seq.getId());
 			
+			String md5Seq = seq.digestSeq();
 	        MessageDigest digest = MessageDigest.getInstance("md5");
 	        digest.reset();
-			String md5 = String.format("%032x",new BigInteger(1,digest.digest(seq.getQualAsString().getBytes())));
+			String md5Qual = String.format("%032x",new BigInteger(1,digest.digest(seq.getQualAsString().getBytes())));
 			log.debug(seq.getId());
-			
-			
-			log.debug("=====> " + seq.getQualAsString());
-			log.debug("=====> " + md5);
-			log.debug("=====> " + chk[1]);
-			assertEquals(chk[1],md5);
+						
+			assertEquals(chk[1],md5Seq);
+			assertEquals(chk[2],md5Qual);
 		}
 	}	
 	
 	@Test
 	public void testReadCSFasta() throws IOException, NoSuchAlgorithmException {
-		testReadFromFile("data/test/sample.csfasta","data/test/sample.qual","data/test/sample.qual.check.txt");
+		testReadFromFile("data/test/ion.fastq.gz","data/test/ion.fastq.check.txt");
 	}
-	*/
 }
