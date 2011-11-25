938c53ddd82f706539dbea7b4cb26db92de88eca
Leonardo Varuzza <varuzza@gmail.com>
2011-11-25 15:41:16 -0200

    Calculate update rate in progressBar

--- bfx-tools/src/main/java/bfx/tools/cli/CLIProgressBar.java
+++ bfx-tools/src/main/java/bfx/tools/cli/CLIProgressBar.java
@@ -6,18 +6,23 @@ import java.util.Observer;
 import bfx.ProgressCounter;
 
 public class CLIProgressBar implements Observer {
-
+	private double rate;
+	
 	public void showProgress(ProgressCounter pc) {
 		double x = pc.getTicks();
 		System.out.print('#');
 		if (((long)x)%60==0)
-			System.out.println(" " + x);
+			System.out.println(String.format(" count=%.0f rate=%.2e",x,rate));
 		System.out.flush();
 	}
 
 	@Override
-	public void update(Observable o, Object arg) {
-		showProgress((ProgressCounter)o);
+	public void update(Observable o, Object arg) {		
+		ProgressCounter pc=(ProgressCounter)o;
+		long now = System.currentTimeMillis();
+		rate = pc.getCount() / now; 
+
+		showProgress(pc);
 	}
 
 }
