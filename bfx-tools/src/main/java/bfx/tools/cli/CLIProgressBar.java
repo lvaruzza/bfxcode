aa5d8490cf5acaf1a364cf407c6967bdf6bfa188
Leonardo Varuzza <varuzza@gmail.com>
2011-11-25 17:39:41 -0200

    update ticks only in a newline of the bar.

--- bfx-tools/src/main/java/bfx/tools/cli/CLIProgressBar.java
+++ bfx-tools/src/main/java/bfx/tools/cli/CLIProgressBar.java
@@ -6,10 +6,12 @@ import java.util.Observer;
 import bfx.ProgressCounter;
 
 public class CLIProgressBar implements Observer {
-	private double rate;
 	private long start = -1;
 	
 	public void showProgress(ProgressCounter pc) {
+		if (start==-1)
+			start = System.currentTimeMillis();
+		
 		if (pc.isFinished()) {
 			System.out.println();
 			return;
@@ -17,25 +19,20 @@ public class CLIProgressBar implements Observer {
 			
 		double x = pc.getTicks();
 		System.out.print('#');
-		if (((long)(x+1))%60==0)
-			System.out.println(String.format(" count=%.0f ticks=%.0f rate=%.2e",
-					pc.getCount(),x,rate));
+		if (((long)(x+1))%60==0) {
+			long now = System.currentTimeMillis();
+			long ticks  = (long) (10000.0 * (start - now) / pc.getCount());
+			System.out.println(String.format(" count=%.0f ticks=%.0f newticks=%d",
+					pc.getCount(),x,ticks));
+			pc.setTick(ticks);
+		}
 		System.out.flush();
 	}
 
 	@Override
 	public void update(Observable o, Object arg) {		
 		ProgressCounter pc=(ProgressCounter)o;
-		long now = System.currentTimeMillis();
-		if (start == -1) 
-			start = now;
-		else {
-			if (pc.getCount() > 10) {
-				rate = pc.getCount() / (now-start); 
-				pc.setTick((long)(10000.0/rate));
-			}
-		}
-		showProgress(pc);
+		showProgress(pc);		
 	}
 
 }
