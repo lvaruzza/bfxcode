531be85295561f6dd333a102e4e1acd36b08413a
Leonardo Varuzza <varuzza@gmail.com>
2011-11-25 23:38:51 -0200

    Change initial tick to 1

--- bfx-core/src/main/java/bfx/ProgressCounter.java
+++ bfx-core/src/main/java/bfx/ProgressCounter.java
@@ -3,7 +3,7 @@ package bfx;
 import java.util.Observable;
 
 public  class ProgressCounter extends Observable {
-	private long tick = 10;
+	private long tick = 1;
 	private long count = 0;
 	private boolean finish = false;
 	
