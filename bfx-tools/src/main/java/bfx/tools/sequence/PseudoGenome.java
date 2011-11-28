a3371e615d6e73cbe6bf6747f8c8891917a3098c
Leonardo Varuzza <varuzza@gmail.com>
2011-11-28 16:27:10 -0200

    PseudoGenome generating the concatenated genome.

--- bfx-tools/src/main/java/bfx/tools/sequence/PseudoGenome.java
+++ bfx-tools/src/main/java/bfx/tools/sequence/PseudoGenome.java
@@ -1,7 +1,13 @@
 package bfx.tools.sequence;
 
+import java.io.File;
+
 import bfx.Sequence;
+import bfx.SequenceBuilder;
 import bfx.SequenceSet;
+import bfx.impl.SequenceBuilderListImpl;
+import bfx.io.SequenceWriter;
+import bfx.io.impl.FastaSequenceWriter;
 import bfx.tools.Tool;
 
 import com.beust.jcommander.Parameter;
@@ -15,17 +21,25 @@ public class PseudoGenome extends Tool {
 	public String inputFormat;
 	
 	@Parameter(names = {"--outputGenome","-g"}, description = "Output Genome")
-	public String output;
+	public String outputGenome;
 
+	@Parameter(names = {"--genomeName","-n"}, description = "Genome Name")
+	public String name;
+	
 	@Parameter(names = {"--outputAnnotation","-a"}, description = "Output Annotation")
 	public String annotation;
 	
 	@Override
 	public void run() throws Exception {
 		SequenceSet sequences = SequenceSet.fromFile(input,inputFormat);
+		SequenceBuilder sb = new SequenceBuilderListImpl();
+		
 		for(Sequence s: sequences) {
-			
+			sb.append(s);
 		}
+		Sequence r = sb.getConstQual(name, (byte)0);
+		SequenceWriter sw = new FastaSequenceWriter();
+		sw.write(new File(outputGenome), r);
 	}
 
 	@Override
