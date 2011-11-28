f71420697a696e72a2673fd5f147c517e8201ef1
Leonardo Varuzza <varuzza@gmail.com>
2011-11-28 00:45:24 -0200

    add static fromFile methods in sequenceSet class

--- bfx-core/src/main/java/bfx/SequenceSet.java
+++ bfx-core/src/main/java/bfx/SequenceSet.java
@@ -1,7 +1,9 @@
 package bfx;
 
+import java.io.File;
 import java.util.Iterator;
 
+import bfx.io.impl.FileSequenceSet;
 import bfx.utils.IteratorUtils;
 
 public abstract class SequenceSet implements Iterable<Sequence> {
@@ -12,4 +14,20 @@ public abstract class SequenceSet implements Iterable<Sequence> {
 	public long count() {
 		return IteratorUtils.count(iterator());
 	}
+	
+	public static SequenceSet fromFile(String format,File file) {
+		return new FileSequenceSet(format,file);
+	}	
+
+	public static SequenceSet fromFile(String format,String filename) {
+		return new FileSequenceSet(format,new File(filename));
+	}	
+	
+	public static SequenceSet fromFile(String format,File file1,File file2) {
+		return new FileSequenceSet(format,file1,file2);
+	}		
+
+	public static SequenceSet fromFile(String format,String filename1,String filename2) {
+		return new FileSequenceSet(format,new File(filename1),new File(filename2));
+	}		
 }
