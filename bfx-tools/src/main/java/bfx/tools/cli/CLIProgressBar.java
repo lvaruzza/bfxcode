13562e8eda7b662d0b64d181f64e8c10e2918260
Leonardo Varuzza <varuzza@gmail.com>
2011-12-04 12:24:24 -0200

    Update convert tool and the progressbar

--- bfx-tools/src/main/java/bfx/tools/cli/CLIProgressBar.java
+++ bfx-tools/src/main/java/bfx/tools/cli/CLIProgressBar.java
@@ -3,10 +3,15 @@ package bfx.tools.cli;
 import java.util.Observable;
 import java.util.Observer;
 
+import org.joda.time.Period;
+import org.joda.time.format.PeriodFormat;
+import org.joda.time.format.PeriodFormatter;
+
 import bfx.ProgressCounter;
 
 public class CLIProgressBar implements Observer {
 	private long start = -1;
+	private static PeriodFormatter periodFormatter = PeriodFormat.getDefault();
 	
 	public void showProgress(ProgressCounter pc) {
 		if (start==-1)
@@ -21,10 +26,13 @@ public class CLIProgressBar implements Observer {
 		System.out.print('#');
 		if (((long)(x+1))%60==0) {
 			long now = System.currentTimeMillis();
-			long rate  = (long) (10000.0 * (start - now) / pc.getCount());
-			System.out.println(String.format(" count=%.0f ticks=%.0f rate=%d",
-					pc.getCount(),x,rate));
-			pc.setUpdateRate(rate);
+			long elapsed = now - start;
+			Period period = new Period(elapsed);
+			double rate  = (1000.0 * pc.getCount()) / elapsed;
+			System.out.println(String.format(" %.0f records, elapsed=%s, rate=%.1f rec/s",
+					pc.getCount(), periodFormatter.print(period),rate));
+			
+			pc.setUpdateRate((long)(10*rate));
 		}
 		System.out.flush();
 	}
