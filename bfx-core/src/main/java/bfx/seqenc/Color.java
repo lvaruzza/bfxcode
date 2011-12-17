b0d27e5ffab644080dd5b5c6658bcf8e25ef41e1
Leonardo Varuzza <varuzza@gmail.com>
2011-12-17 18:49:44 -0200

    Add TestColor Finish naive color encoder and decoder.

--- bfx-core/src/main/java/bfx/seqenc/Color.java
+++ bfx-core/src/main/java/bfx/seqenc/Color.java
@@ -1,5 +1,7 @@
 package bfx.seqenc;
 
+import org.apache.log4j.Logger;
+
 import bfx.Sequence;
 
 
@@ -10,7 +12,8 @@ import bfx.Sequence;
 // T 3 2 1 0
 
 public class Color {
-
+	private static Logger log = Logger.getLogger(Color.class);
+	
 	private static byte[] enc = new byte[] {
 		4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 
         4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 
@@ -27,6 +30,7 @@ public class Color {
         4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4};
 
 	static byte[] bases = new byte[] {'A','C','G','T','N'};
+	static byte[] colors = new byte[] {'0','1','2','3','.'};
 	
 /*	{{0,1,2,3,4},
    	 {1,0,3,2,4},
@@ -35,17 +39,21 @@ public class Color {
    	 {4,4,4,4,4}};
 */
 	
-	static byte[][] covi = new byte[][] 
+	static byte[][] mat = new byte[][] 
 							{{0,1,2,3,4},
 				        	 {1,0,3,2,4},
 				        	 {2,3,0,1,4},
 				        	 {3,2,1,0,4},
 				        	 {4,4,4,4,4}};
-
-	public static byte naiveColorDecodeBase(byte a,byte b) {
-		return bases[covi[a][b]];
-	}
 	
+	public static byte naiveColorDecodeBase(byte a,byte b) {
+		return bases[mat[enc[a]][enc[b]]];
+	}
+
+	public static byte naiveColorEncodeColor(byte a,byte b) {
+		return colors[mat[enc[a]][enc[b]]];
+	}
+
 	public static byte[] naiveColorDecode(byte[] seq,int start,int length) {
 		byte[] r = new byte[length];
 		r[0]=seq[start];
@@ -55,12 +63,22 @@ public class Color {
 		return r;
 	}
 
+	public static byte[] naiveColorEncode(byte[] seq,int start,int length) {
+		byte[] r = new byte[length];
+		r[0]=seq[start];
+		for(int i=start+1,j=1;i<start+length;i++,j++) {
+			//log.debug(String.format("i=%d j=%d",i,j));
+			r[j] = naiveColorEncodeColor(seq[i-1],seq[i]);
+		}
+		return r;
+	}
+	
 	public static byte[] colorDecode(byte[] seq) {
-		return null;
+		return naiveColorDecode(seq,0,seq.length);
 	}
 
 	public static byte[] colorEncode(byte[] seq) {
-		return null;
+		return naiveColorEncode(seq,0,seq.length);
 	}
 	
 	public static Sequence colorDecode(Sequence seq) {
