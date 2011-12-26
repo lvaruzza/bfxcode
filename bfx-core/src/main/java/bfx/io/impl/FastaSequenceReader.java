d625497dc42f76ffe326995642866c454cfc0cc8
Leonardo Varuzza <varuzza@gmail.com>
2011-12-26 02:35:12 -0200

    Test fasta/qual filenames before reading it.

--- bfx-core/src/main/java/bfx/io/impl/FastaSequenceReader.java
+++ bfx-core/src/main/java/bfx/io/impl/FastaSequenceReader.java
@@ -1,16 +1,23 @@
 package bfx.io.impl;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Reader;
 import java.util.Iterator;
 
+import org.apache.commons.io.FilenameUtils;
+import org.apache.log4j.Logger;
+
 import bfx.ProgressCounter;
 import bfx.Sequence;
 import bfx.io.SequenceReader;
+import bfx.utils.compression.CompressionUtils;
 import bfx.utils.io.BaseSingleAndDualReader;
 
 public class FastaSequenceReader extends BaseSingleAndDualReader<Iterator<Sequence>> implements SequenceReader {
+	private static Logger log = Logger.getLogger(FastaSequenceReader.class);
+	
 	private byte defaultQuality = 0;
 	
 	public FastaSequenceReader() {};
@@ -28,6 +35,28 @@ public class FastaSequenceReader extends BaseSingleAndDualReader<Iterator<Sequen
 		return new LineBasedFastaIterator(fastaReader,defaultQuality);
 	}
 
+	@Override
+	public Iterator<Sequence> read(File file1,File file2) throws IOException{
+		if(file1.equals(file2)) {
+			throw new IOException(String.format("Sequence file '%s', should be different from qual file '%s'",file1,file2));
+		}
+		// TODO: Test the file names 
+		return super.read(file1,file2);
+	}
+	
+	@Override
+	public Iterator<Sequence> read(String filename1,String filename2) throws IOException{
+		if(filename1.equals(filename2)) {
+			throw new IOException(String.format("Sequence file '%s', should be different from qual file '%s'",filename1,filename2));
+		}
+			
+		//String ext1 = FilenameUtils.getExtension(CompressionUtils.uncompressedFilename(filename1));
+		String ext2 = FilenameUtils.getExtension(CompressionUtils.uncompressedFilename(filename1));
+		if (!ext2.equals("qual"))
+			log.warn(String.format("Suspicious extension in qual file: '%s'. Fasta qual file normally has '.qual' extension.",ext2));
+		return super.read(filename1,filename2);
+	}
+	
 	@Override
 	public Iterator<Sequence> read(InputStream fastaInput, InputStream qualInput)
 			throws IOException {
