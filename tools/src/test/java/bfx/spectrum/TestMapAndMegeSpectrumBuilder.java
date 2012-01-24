0126c8c36bf341a1aa8ea3df567d61e26dba8744
Leonardo Varuzza <varuzza@gmail.com>
2012-01-24 08:35:03 -0200

    Test with java 7

--- tools/src/test/java/bfx/spectrum/TestMapAndMegeSpectrumBuilder.java
+++ tools/src/test/java/bfx/spectrum/TestMapAndMegeSpectrumBuilder.java
@@ -15,6 +15,8 @@ public class TestMapAndMegeSpectrumBuilder {
 	public void test0() throws IOException {
 		MapAndMegeSpectrumBuilder sb = new MapAndMegeSpectrumBuilder(3,10);
 		sb.add(SequenceSource.fromFile("fasta","data/test/spectrumU3.fasta"));	
+		sb.finish();
+		
 		log.debug(TextUtils.doubleLine());
 		sb.mergeLevel(0, sb.getNparts());
 		log.debug(TextUtils.doubleLine());
@@ -23,8 +25,19 @@ public class TestMapAndMegeSpectrumBuilder {
 		sb.mergeLevel(2, sb.getNparts()/4);
 		log.debug(TextUtils.doubleLine());
 		sb.mergeLevel(3, sb.getNparts()/8);
+
+		MemorySpectrumBuilder msb = new MemorySpectrumBuilder(3);
+		msb.add(SequenceSource.fromFile("fasta","data/test/spectrumU3.fasta"));	
+		msb.finish();
+		msb.save("fromMemory.spec");
 		
-		DiskSpectrum dsk = new DiskSpectrum(sb.getPartName(4,0));
-		dsk.dump(System.out);
+		DiskSpectrum dsk1 = new DiskSpectrum(sb.getPartName(4,0));
+		DiskSpectrum dsk2 = new DiskSpectrum("fromMemory.spec");
+
+		dsk1.dump(System.out);
+		System.out.println(TextUtils.doubleLine());
+		dsk2.dump(System.out);
+		
+		
 	}
 }
