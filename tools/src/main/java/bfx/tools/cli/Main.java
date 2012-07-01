22e220b6767978858772f63c21a87968d2f9a532
Leonardo Varuzza <varuzza@gmail.com>
2012-06-30 22:06:05 -0300

    Add logback configurationfile

--- tools/src/main/java/bfx/tools/cli/Main.java
+++ tools/src/main/java/bfx/tools/cli/Main.java
@@ -26,7 +26,7 @@ public class Main {
 	private static Map<String,Class<? extends Tool>> commands = new HashMap<String,Class<? extends Tool>>();;
 	
 	public static void addCommand(String name,Class<? extends Tool> klass) {
-		System.out.println(String.format("Registering command '%s' to class '%s'",name,klass.getName()));
+		System.err.println(String.format("Registering command '%s' to class '%s'",name,klass.getName()));
 		commands.put(name,klass);
 	}
 	
@@ -52,6 +52,13 @@ public class Main {
 		}
 	}
 	
+	/**
+	 * Create a instance of klass, pass args and run it
+	 * 
+	 * @param klass - A class which extends Tool
+	 * @param args - Commnad line argnuments
+	 * 
+	 */
 	public static void run(Class<? extends Tool> klass,String... args) {
 		try {
 			log.info(String.format("Loading class '%s'",klass.getName()));
@@ -69,7 +76,8 @@ public class Main {
 			e.printStackTrace();
 		}
 	}
-	/*
+	/**
+	 * 
 	 * The first argument is the tool to be executed.
 	 * 
 	 * Create a instance of the proper class and run it.
