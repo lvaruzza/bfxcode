4f05cecccd5def3b9fde1355dfa8566d67b9ff57
Leonardo Varuzza <varuzza@gmail.com>
2012-07-24 10:17:54 -0300

    update core version improve make-dist.sh

--- tools/make-dist.sh
+++ tools/make-dist.sh
@@ -1,5 +1,8 @@
-mkdir -p bfxtools
-cp -Rv target/lib bfxtools
-cp -Rv target/tools*.jar bfxtools
-cp bfx.dist bfxtools/bfx
-tar cjvf bfxtools.tar.bz2 bfxtools
+version=`cat VERSION`
+#rm -Rf bfxtools-*
+#mkdir -p bfxtools-$version
+#cp -Rv target/lib bfxtools-$version
+#cp -Rv target/tools*.jar bfxtools-$version
+sed "s/@VERSION@/$version/g" bfx.dist > bfxtools-$version/bfx
+chmod +x bfxtools-$version/bfx
+#tar cjvf bfxtools-$version.tar.bz2 bfxtools-$version
