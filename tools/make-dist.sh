1f3d4ee0ac8c873e75f11d3de200e701459cf745
Leonardo Varuzza <varuzza@gmail.com>
2012-07-24 10:33:13 -0300

    Update dependency in tools improve make-dist.sh

--- tools/make-dist.sh
+++ tools/make-dist.sh
@@ -1,8 +1,13 @@
 version=`cat VERSION`
-#rm -Rf bfxtools-*
-#mkdir -p bfxtools-$version
-#cp -Rv target/lib bfxtools-$version
-#cp -Rv target/tools*.jar bfxtools-$version
-sed "s/@VERSION@/$version/g" bfx.dist > bfxtools-$version/bfx
+mkdir dist
+cd dist
+rm -Rf bfxtools-*
+if [ -d bfxtools-$version ]; then
+ mkdir -p bfxtools-$version
+fi
+cp -Rv ../target/lib bfxtools-$version
+cp -Rv ../target/tools*.jar bfxtools-$version
+sed "s/@VERSION@/$version/g" ../bfx.dist > bfxtools-$version/bfx
 chmod +x bfxtools-$version/bfx
-#tar cjvf bfxtools-$version.tar.bz2 bfxtools-$version
+tar cjvf bfxtools-$version.tar.bz2 bfxtools-$version
+rm -Rf bfxtools-$version
