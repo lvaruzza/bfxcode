6ca78734618d19e660c94e146e5452e5352e725a
Leonardo Varuzza <varuzza@gmail.com>
2012-08-20 12:52:51 -0300

    Add CutNCBINames

---
+++ tools/src/main/java/bfx/tools/sequence/CutNCBINames.java
@@ -0,0 +1,63 @@
+package bfx.tools.sequence;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import bfx.Sequence;
+import bfx.io.SequenceSink;
+import bfx.io.SequenceSource;
+import bfx.io.impl.FileSequenceSink;
+import bfx.io.impl.FileSequenceSource;
+import bfx.process.ProgressMeter;
+import bfx.tools.Tool;
+import bfx.utils.TextUtils;
+
+import com.beust.jcommander.Parameter;
+
+public class CutNCBINames extends Tool {
+	private static Logger log = LoggerFactory.getLogger(CutNCBINames.class);
+	
+	@Parameter(names = {"--input","-i"}, description = "Input File",required=true)
+	public String input;
+
+	@Parameter(names = {"--qual","-q"}, description = "Qual file (only applicable for fasta format)")
+	public String qual;
+	
+	@Parameter(names = {"--inputFormat","-if"}, description = "Input Format")
+	public String inputFormat;
+	
+	@Parameter(names = {"--output","-o"}, description = "Output File")
+	public String output;
+
+	@Parameter(names = {"--outputQual","-oq"}, description = "Output Qual File (only appliable for fasta format)")
+	public String outputQual;
+	
+	@Parameter(names = {"--outputFormat","-of"}, description = "Output Format")
+	public String outputFormat;
+	
+	@Override
+	public void run() throws Exception {
+		SequenceSource src = new FileSequenceSource(inputFormat,input,qual);
+		SequenceSink sink =  new FileSequenceSink(outputFormat,output,outputQual);
+		
+		ProgressMeter pm = getProgressMeterFactory().get();
+		
+		src.setProgressMeter(pm);
+		for(Sequence seq: src) {
+			String[] parts = seq.getId().split("|");
+			Sequence result = seq.changeHeader(parts[3],"");
+			sink.write(result);
+		}
+		pm.finish();
+		
+		log.info(TextUtils.doubleLine());
+		log.info("Finished.");
+		log.info(TextUtils.doubleLine());
+	}
+
+	@Override
+	public String getName() {
+		return "fixncbi";
+	}
+
+}
