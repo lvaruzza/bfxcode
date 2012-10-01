1be5a8d04637d4d39414aae8f0b52ff9711d16c1
Leonardo Varuzza <varuzza@gmail.com>
2012-10-01 10:17:41 -0300

    Make IR temporary compilable

--- tools/src/main/scala/bfx/tools/sequence/filter/IR.scala
+++ tools/src/main/scala/bfx/tools/sequence/filter/IR.scala
@@ -4,6 +4,10 @@ abstract case class IR {
   def typ:Symbol;
 }
 
+case class Unimplemented extends IR {
+  def typ = 'Unknown
+}
+
 abstract case class NamedIR extends IR {
   def name:Symbol;
 }
@@ -41,20 +45,37 @@ object IRFunction extends IRUtils {
 
 case class IRConst(value:Float,override val typ:Symbol) extends IR
 
-case class IRBinaryOP(op:Symbol,left:IR,right:IR,method:String,typ:Symbol) extends NamedIR {
+case class IROperator(op:Symbol,typ:Symbol,operator:String) extends NamedIR {
   def name = op
+}
+
+object IROperator extends IRUtils {
+    var operators = makeMap(IROperator('and,'boolean,"&&"))
+
+    def get(op:Symbol) = operators(op)
+}
+case class IRBinaryOP(op:IROperator,left:IR,right:IR) extends IR {
+  def typ = op.typ
+}
+
+object IRBinaryOP { 
+	def make(op:Symbol,left:IR,right:IR) = {
+	  val iop = IROperator.get(op)
+	  IRBinaryOP(iop,left,right)
+	  
+	}
 }
 
 
 object IR {
-  def compile1(e: Expr):Any = {
+  def compile1(e: Expr):IR = {
     
     val r = e match {
       case e: Variable => IRVariable.get(e.name)
       case e: Number => IRConst(e.value,'number)
-      case e: BinaryOp => e //IRBinaryOP(compile1(e.left), e.operator.name, compile1(e.right))
-      case e: Function => e // Functions.get(e.name.name, compile(e.arg))
-      case e: UnaryOp => e //URUnaryOp(e.operator, compile(e.arg))
+      case e: BinaryOp => IRBinaryOP.make(e.operator,compile1(e.left),compile1(e.right))
+      case e: Function => Unimplemented() // IRFunction(e.op,compile1(e.arg))
+      case e: UnaryOp => Unimplemented() //IRUnaryOp(e.op,compile1(e.arg))
     }
     r
   }  
