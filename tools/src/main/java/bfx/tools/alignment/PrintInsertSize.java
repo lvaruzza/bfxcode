4c7bdfa3d2a1e484f2f0f11c60af9d0aa0b0b911
Leonardo Varuzza <varuzza@gmail.com>
2013-03-21 15:35:11 -0300

    Print deciles

--- tools/src/main/java/bfx/tools/alignment/PrintInsertSize.java
+++ tools/src/main/java/bfx/tools/alignment/PrintInsertSize.java
@@ -2,20 +2,62 @@ package bfx.tools.alignment;
 
 import java.io.File;
 import java.io.PrintStream;
-import java.util.LinkedList;
-import java.util.List;
+import java.io.PrintWriter;
 
 import net.sf.samtools.SAMFileReader;
 import net.sf.samtools.SAMRecord;
 
 import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
-import org.apache.commons.math3.stat.descriptive.rank.Median;
 
+import scala.actors.threadpool.Arrays;
+
+import bfx.tools.Report;
 import bfx.tools.Tool;
 
 import com.beust.jcommander.Parameter;
 
 public class PrintInsertSize extends Tool {
+	public static class InsertStatReport extends Report {
+		double q1;
+		double median;
+		double[] decils;
+		double q3;
+		double iqd;
+		long positive;
+		long negative;
+		long zero;
+		
+		public InsertStatReport(DescriptiveStatistics stats,long positive,long negative, long zero) {
+			q1 = stats.getPercentile(25);
+			median = stats.getPercentile(50);
+			q3 = stats.getPercentile(75);
+			iqd = q3-q1;
+			decils = new double[10];
+			for(int i=10;i<=100;i+=10) {
+				decils[i/10-1]=stats.getPercentile(i); 
+			}
+			this.positive=positive;
+			this.negative=negative;
+			this.zero=zero;
+		}
+
+
+		@Override
+		public void writeHuman(PrintWriter out) {
+			out.println("Non zero insert statistics");
+			out.println(String.format("Q1:         \t%.1f",q1));
+			out.println(String.format("Median:     \t%.1f",median));
+			out.println(String.format("Q3:         \t%.1f",q3));
+			out.println(String.format("IQR:        \t%.1f",iqd));
+			out.println(String.format("deciles:    \t%s",Arrays.toString(decils)));
+			out.println("Insert orientation count");
+			out.println(String.format("pos. count: \t%d",positive));
+			out.println(String.format("neg. count: \t%d",negative));
+			out.println(String.format("zero count: \t%d",zero));
+		}
+		
+	}
+	
 	//private static Logger log = Logger.getLogger(Convert.class);
 	
 	@Parameter(names = {"--input","-i"}, description = "Input File",required=true)
@@ -29,22 +71,26 @@ public class PrintInsertSize extends Tool {
 		SAMFileReader reader = new SAMFileReader(new File(input));
 		PrintStream out = output == null ? null : new PrintStream(output);
 		DescriptiveStatistics stats = new DescriptiveStatistics();
+		long positive=0;
+		long negative=0;
+		long zero=0;
+		
 		for (SAMRecord it:reader) {
 			long x=it.getInferredInsertSize();
 			if (out != null) out.println(x);
-			stats.addValue(Math.abs(x));
+			if (x==0) {
+				zero++;
+			} else if (x>0) {
+				positive++;
+			} else {
+				negative++;
+			}
+			if (x!=0) stats.addValue(Math.abs(x));
 		}
 		if (out!=null) out.close();
 		reader.close();
-		
-		double q1 = stats.getPercentile(25);
-		double q2 = stats.getPercentile(50);
-		double q3 = stats.getPercentile(75);
-		
-		System.out.println(q1);
-		System.out.println(q2);
-		System.out.println(q3);
-		System.out.println(q3-q1);
+		InsertStatReport report = new InsertStatReport(stats,positive,negative,zero);
+		report.write(System.out, "human");
 	}
 
 	@Override
