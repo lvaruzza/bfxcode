49762432f6065f15ea6aaf633f8b66f8f963f9b5
Leonardo Varuzza <varuzza@gmail.com>
2013-04-12 15:47:11 -0300

    Add BAMSequenceReader

--- core/src/main/java/bfx/io/impl/BAMSequenceReader.java
+++ core/src/main/java/bfx/io/impl/BAMSequenceReader.java
@@ -5,41 +5,70 @@ import java.io.InputStream;
 import java.io.Reader;
 import java.util.Iterator;
 
+import net.sf.samtools.SAMFileReader;
+import net.sf.samtools.SAMRecord;
 import bfx.Sequence;
+import bfx.impl.SequenceQual;
 import bfx.io.SequenceReader;
 
 public class BAMSequenceReader extends SequenceReader {
+	public static class BAMIterator implements Iterator<Sequence> {
+		private Iterator<SAMRecord> samit;
+		private SAMFileReader reader;
+		
+		public BAMIterator(InputStream in) {
+			reader = new SAMFileReader(in);
+			samit = reader.iterator();
+		}
+		
+		@Override
+		public boolean hasNext() {
+			if (!samit.hasNext())
+				reader.close();
+			
+			return samit.hasNext();
+		}
 
+		@Override
+		public Sequence next() {
+			SAMRecord align = samit.next();
+			return new SequenceQual(align.getReadName(), 
+					align.getReadBases(),
+					align.getBaseQualities());
+		}
+
+		@Override
+		public void remove() {
+			throw new RuntimeException("Unimplemented");
+		}
+		
+	}
+	
 	@Override
 	public String getFormatName() {
-		// TODO Auto-generated method stub
-		return null;
+		return "BAM";
 	}
 
 	@Override
 	public Iterator<Sequence> read(InputStream in) throws IOException {
-		// TODO Auto-generated method stub
-		return null;
+		return new BAMIterator(in);
 	}
 
 	@Override
 	public Iterator<Sequence> read(Reader in) throws IOException {
-		// TODO Auto-generated method stub
-		return null;
+		throw new RuntimeException("Unimplemented");
 	}
 
 	@Override
 	public Iterator<Sequence> read(InputStream input1, InputStream input2)
 			throws IOException {
-		// TODO Auto-generated method stub
-		return null;
+		return new BAMIterator(input1);
 	}
 
 	@Override
 	public Iterator<Sequence> read(Reader reader1, Reader reader2)
 			throws IOException {
-		// TODO Auto-generated method stub
-		return null;
+		throw new RuntimeException("Unimplemented");
 	}
 
 }
--- core/src/main/java/bfx/utils/Pair.java
+++ core/src/main/java/bfx/utils/Pair.java
@@ -23,4 +23,45 @@ public class Pair<F,S> {
 		this.fst = fst;
 		this.snd = snd;
 	}
+
+
+	@Override
+	public String toString() {
+		return "Pair [fst=" + fst + ", snd=" + snd + "]";
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((fst == null) ? 0 : fst.hashCode());
+		result = prime * result + ((snd == null) ? 0 : snd.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pair other = (Pair) obj;
+		if (fst == null) {
+			if (other.fst != null)
+				return false;
+		} else if (!fst.equals(other.fst))
+			return false;
+		if (snd == null) {
+			if (other.snd != null)
+				return false;
+		} else if (!snd.equals(other.snd))
+			return false;
+		return true;
+	}
+	
+	
 }
