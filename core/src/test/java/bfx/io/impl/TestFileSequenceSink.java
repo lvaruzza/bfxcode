c7deafe0477039e0158d13e7c772e67d8aae5564
Leonardo Varuzza <varuzza@gmail.com>
2013-12-09 11:20:06 -0200

    Remove warnings

--- core/src/test/java/bfx/io/impl/TestFileSequenceSink.java
+++ core/src/test/java/bfx/io/impl/TestFileSequenceSink.java
@@ -16,5 +16,7 @@ public class TestFileSequenceSink {
 	public void testConstructor() {
 		SequenceSink sink1 = new FileSequenceSink("fasta","file1.fasta","file1.qual");
 		SequenceSink sink2 = new FileSequenceSink("file1.fasta","file1.qual");
+		assertNotNull(sink1);
+		assertNotNull(sink2);
 	}
 }
